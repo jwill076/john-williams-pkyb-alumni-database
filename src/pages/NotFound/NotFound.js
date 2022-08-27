8dce72a6c0be0550c9075f56d9c935c36ed38f1d
John Williams <jw@mlmpipa.org>
2022-08-27 11:19:21 -0400

    Initial Folder Structure with Pages & Assets

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.scss";
+
+function NotFound() {
+    return(
+        <main className="not-found">
+            <h1>404 Type Error</h1>
+            <p>Page Not Found</p>
+        </main>
+    )
+}
+
+export default NotFound;
